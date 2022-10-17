a1ff12e31dedf7cabce3e2da9d2797cf7f30c458
Mohamed Hussein El-adwy <mohamed.h.eladwy@gmail.com>
2022-10-17 22:08:16 +0200

    update the solution of problem Password

--- A/Password.cpp
+++ A/Password.cpp
@@ -1,7 +1,5 @@
 #include <bits\stdc++.h>
 using namespace std;
-
-typedef set<int> setint;
 #define endl '\n'
 
 int main()
@@ -17,13 +15,9 @@ int main()
     {
         int n, num;
         cin >> n;
-        setint allNumbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int i = 0; i < n; i++)
-        {
             cin >> num;
-            allNumbers.erase(num);
-        }
-        cout << 6 * ((allNumbers.size() * (allNumbers.size() - 1)) / 2) << endl;
+        cout << 6 * (((10 - n) * (9 - n)) / 2) << endl;
     }
 
     return 0;
